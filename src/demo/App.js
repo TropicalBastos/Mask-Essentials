e8bf760d9d6f18fede707f8be10c14b9f0761e81
TropicalBastos <ian-bastos@live.com>
2017-11-05 17:22:38 +0000

    integrated jsillitoe's currencymask

--- src/demo/App.js
+++ src/demo/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import NumberMask from '../lib';
 import { CharMask } from '../lib';
 import { IsoDateMask } from '../lib';
+import { CurrencyMask } from '../lib';
 
 const App = () => (
   <div>
     <NumberMask />
     <CharMask />
     <IsoDateMask />
+    <CurrencyMask prefix="£" />
   </div>
 );
 
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,11 +1,13 @@
 import NumberMask from './components/NumberMask';
 import CharMask from './components/CharMask';
 import IsoDateMask from './components/IsoDateMask';
+import CurrencyMask from './components/currencymask/CurrencyMask'
 
 export default NumberMask;
 export {
   NumberMask,
   CharMask,
-  IsoDateMask
+  IsoDateMask,
+  CurrencyMask
 };
 
